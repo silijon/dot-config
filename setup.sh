2a82e2f5fa13b360f39396e62412fc0b8c9f19a3
John Dennis <john.w.dennis@gmail.com>
2014-10-13 20:34:58 -0700

    updated setup

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ ln -s ~/dot-config/.bash_profile ~/.bash_profile
 ln -s ~/dot-config/.vim ~/.vim
 ln -s ~/dot-config/.vimrc ~/.vimrc
 ln -s ~/dot-config/.tmux.conf ~/.tmux.conf
+ln -s ~/dot-config/.gitconfig ~/.gitconfig
