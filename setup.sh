7f8e47f0d5efe4b0f334b15ddbbff54a014d274f
silijon <john.w.dennis@gmail.com>
2014-10-21 10:56:36 -0700

    shoring up submodules and crlf

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-ln -s ~/dot-config/.bash_profile ~/.bash_profile
+ln -s ~/dot-config/.bash_profile ~/.bash_profile 
 ln -s ~/dot-config/.vim ~/.vim
 ln -s ~/dot-config/.vimrc ~/.vimrc
 ln -s ~/dot-config/.tmux.conf ~/.tmux.conf
 ln -s ~/dot-config/.gitconfig ~/.gitconfig
-ln -s ~/dot-config/git-prompt.sh ~/git-prompt.sh
+
+git submodule update --init .vim/bundle/nerdtree
+git submodule update --init .vim/bundle/nerdcommenter
