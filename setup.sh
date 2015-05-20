1048d2593174d2c3bbd767708b14e853e2c74a25
JD <john.dennis@skykick.com>
2015-05-20 15:47:30 -0700

    added dbext

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ ln -s ~/dot-config/.gitconfig ~/.gitconfig
 
 git submodule update --init .vim/bundle/nerdtree
 git submodule update --init .vim/bundle/nerdcommenter
+git submodule update --init .vim/bundle/dbext.vim
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o /etc/git-completion.bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o /etc/git-prompt.sh
