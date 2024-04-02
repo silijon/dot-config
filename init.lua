97cff0a99c1848e8217d11320f8f1a570286e640
silijon <john.w.dennis@gmail.com>
2024-04-02 13:31:26 -0700

    added logging level to mason for debugging lsp installation failures

--- init.lua
+++ init.lua
@@ -611,7 +611,9 @@ require('lazy').setup {
       --    :Mason
       --
       --  You can press `g?` for help in this menu
-      require('mason').setup()
+      require('mason').setup({
+        log_level = vim.log.levels.INFO -- change me to DEBUG if lsp won't install properly
+      })
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
