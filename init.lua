152027c79ee0911f1617b05374ef34bf5a8cb895
silijon <john.w.dennis@gmail.com>
2024-05-07 14:23:39 -0700

    fixed encoding issue between clangd and copilot

--- init.lua
+++ init.lua
@@ -558,7 +558,11 @@ require('lazy').setup {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
+        clangd = {
+          capabilities = {
+            offsetEncoding = { 'utf-16' }, -- make it play nice with copilot
+          }
+        },
         cmake = {},
         html = {
           filetypes = { "html", "templ", "jinja" },
