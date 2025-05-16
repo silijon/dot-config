8078e20bc9a663a486bcfbb0c8fbacf335110dc7
silijon <john.w.dennis@gmail.com>
2025-05-16 16:25:43 -0700

    Disables pedantic warning in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -136,6 +136,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+---@diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
