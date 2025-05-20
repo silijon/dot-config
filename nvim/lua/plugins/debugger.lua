7e394f3ab1fb233bad0f897b432128cb172219a5
silijon <john.w.dennis@gmail.com>
2025-05-20 11:09:05 -0700

    Adds js/ts debugging

--- nvim/lua/plugins/debugger.lua
+++ nvim/lua/plugins/debugger.lua
@@ -1,65 +1,168 @@
 return {
   {
-    'mfussenegger/nvim-dap',
-    event = 'VeryLazy',
+    "mfussenegger/nvim-dap",
+    event = "VeryLazy",
     dependencies = {
-      'rcarriga/nvim-dap-ui',
-      'nvim-neotest/nvim-nio',
-      'jay-babu/mason-nvim-dap.nvim',
-      'theHamsta/nvim-dap-virtual-text',
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "jay-babu/mason-nvim-dap.nvim",
+      "theHamsta/nvim-dap-virtual-text",
     },
     config = function()
-      local mason_dap = require('mason-nvim-dap')
-      local dap = require('dap')
-      local ui = require('dapui')
-      local dap_virtual_text = require('nvim-dap-virtual-text')
 
-      dap_virtual_text.setup({})
+      local dap = require("dap")
+      local dap_utils = require("dap.utils")
+      local dapui = require("dapui")
 
-      mason_dap.setup({
-        ensure_installed = { 'python' },
+      dap.set_log_level("DEBUG")
+
+      -- Adds inline variable values (super comfy)
+      require("nvim-dap-virtual-text").setup({})
+
+      -- Install adapters
+      require("mason-nvim-dap").setup({
+        ensure_installed = { "python", "js" },
         automatic_installation = true,
         handlers = {
           function(config)
-            require('mason-nvim-dap').default_setup(config)
+            require("mason-nvim-dap").default_setup(config)
           end,
           python = function(config)
-            local venv = os.getenv('VIRTUAL_ENV') or '/usr'
+            local venv = os.getenv("VIRTUAL_ENV") or "/usr"
             config.adapters = {
-              type = 'executable',
-              command = venv .. '/bin/python',
-              args = { '-m', 'debugpy.adapter' },
+              type = "executable",
+              command = venv .. "/bin/python",
+              args = { "-m", "debugpy.adapter" },
             }
-            require('mason-nvim-dap').default_setup(config)
+            require("mason-nvim-dap").default_setup(config)
           end,
         },
       })
 
+      -- Add addtl adapters
       dap.adapters.remote = function(callback, config)
         callback({
-          type = 'server',
+          type = "server",
           host = config.connect.host,
           port = config.connect.port,
         })
       end
 
-      ui.setup()
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "js-debug-adapter",
+          args = { "${port}", },
+        },
+      }
 
-      dap.listeners.before.attach.dapui_config = function() ui.open({ reset = true })  end
-      dap.listeners.before.launch.dapui_config = function() ui.open({ reset = true })  end
-      dap.listeners.before.event_terminated.dapui_config = ui.close
-      dap.listeners.before.event_exited.dapui_config = ui.close
+      -- Useful links for js/ts
+      -- https://github.com/gonstoll/dotfiles/blob/master/.config/nvim/lua/plugins/dap.lua
+      -- https://github.com/Andrei0872/dotfiles/blob/master/nvim/after/plugin/dap.lua
+      -- https://github.com/vercel/next.js/blob/canary/.vscode/launch.json
+      dap.configurations.javascript = {
+        {
+          name = "Launch File",
+          type = "pwa-node",
+          request = "launch",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+          skipFiles = {
+            "<node_internals>/**",
+            "**/node_modules/**",
+          },
+        },
+      }
 
-      vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = "Toggle [B]reakpoint" })
-      vim.keymap.set('n', '<leader>dc', dap.continue, { desc = "[C]ontinue" })
-      vim.keymap.set('n', '<leader>dn', dap.step_over, { desc = "Step Over" })
-      vim.keymap.set('n', '<leader>di', dap.step_into, { desc = "Step [I]nto" })
-      vim.keymap.set('n', '<leader>do', dap.step_out, { desc = "Step [O]ut" })
-      vim.keymap.set('n', '<leader>dg', dap.goto_, { desc = "[G]oto Current Debug Position" })
-      vim.keymap.set('n', '<leader>dl', dap.run_last, { desc = "Run [L]ast Configuration" })
-      vim.keymap.set('n', '<leader>dx', dap.terminate, { desc = "Terminate" })
-      vim.keymap.set('n', '<leader>dp', function() ui.open({ reset = true }) end, { desc = "O[p]en the Debugger" })
-      vim.keymap.set('n', '<leader>dq', ui.close, { desc = "[Q]uit the Debugger" })
+      dap.configurations.typescript = {
+        {
+          name = "Launch File",
+          type = "pwa-node",
+          request = "launch",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+          runtimeExecutable = "tsx",
+          skipFiles = {
+            "<node_internals>/**",
+            "**/node_modules/**",
+          },
+        },
+      }
+
+      dap.configurations.typescriptreact = {
+        {
+          name = "Launch: Next.js Server",
+          type = "pwa-node",
+          request = "launch",
+          program = "${workspaceFolder}/node_modules/.bin/next",
+          args = { "dev" },
+          cwd = "${workspaceFolder}",
+          webRoot = "${workspaceFolder}",
+          sourceMaps = true,
+          -- sourceMapPathOverrides = {
+          --   ["webpack-internal:///\\(rsc\\)/(?:../)*"] = "${workspaceFolder}/*",
+          -- },
+          skipFiles = {
+            "<node_internals>/**",
+            "**/node_modules/**",
+          },
+        },
+      }
+
+      -- Attach to running node proc for all node file types
+      for _, language in ipairs({ "typescript", "typescriptreact", "javascript", "javascriptreact" }) do
+
+        dap.configurations[language] = dap.configurations[language] or {}
+
+        table.insert(dap.configurations[language], {
+          type = "pwa-node",
+          request = "attach",
+          name = "Attach: Running Process",
+          processId = dap_utils.pick_process,
+          cwd = "${workspaceFolder}",
+          sourceMaps = true,
+          skipFiles = {
+            "<node_internals>/**",
+            "**/node_modules/**",
+          },
+        })
+
+        table.insert(dap.configurations[language], {
+          name = "Attach: Node Inspector",
+          type = "pwa-node",
+          request = "attach",
+          cwd = "${workspaceFolder}",
+          port = 9230,
+          sourceMaps = true,
+          skipFiles = {
+            "<node_internals>/**",
+            "**/node_modules/**",
+          },
+        })
+
+      end
+
+
+      -- UI
+      dapui.setup()
+      dap.listeners.before.attach.dapui_config = function() dapui.open({ reset = true })  end
+      dap.listeners.before.launch.dapui_config = function() dapui.open({ reset = true })  end
+      dap.listeners.before.event_terminated.dapui_config = dapui.close
+      dap.listeners.before.event_exited.dapui_config = dapui.close
+
+      -- Keybindings
+      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle [B]reakpoint" })
+      vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "[C]ontinue" })
+      vim.keymap.set("n", "<leader>dn", dap.step_over, { desc = "Step Over" })
+      vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step [I]nto" })
+      vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "Step [O]ut" })
+      vim.keymap.set("n", "<leader>dg", dap.goto_, { desc = "[G]oto Current Debug Position" })
+      vim.keymap.set("n", "<leader>dl", dap.run_last, { desc = "Run [L]ast Configuration" })
+      vim.keymap.set("n", "<leader>dx", dap.terminate, { desc = "Terminate" })
+      vim.keymap.set("n", "<leader>dp", function() dapui.open({ reset = true }) end, { desc = "O[p]en the Debugger" })
+      vim.keymap.set("n", "<leader>dq", dapui.close, { desc = "[Q]uit the Debugger" })
 
       vim.api.nvim_set_hl(0, "DapStoppedLinehl", { bg = "#555530" })
       vim.fn.sign_define("DapStopped", { linehl = "DapStoppedLinehl" })
